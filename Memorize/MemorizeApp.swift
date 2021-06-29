c36854ec7976fab7b55340726e470a6aec1f03de
hisDudeness95 <57687503+hisDudeness95@users.noreply.github.com>
2021-06-29 10:41:50 -0600

    Initial Commit

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by His Dudeness on 6/29/21.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
