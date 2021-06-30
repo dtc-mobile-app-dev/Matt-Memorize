0e45154c0bd106ed89ee6dbbc80573db8f3a8002
hisDudeness95 <57687503+hisDudeness95@users.noreply.github.com>
2021-06-30 11:41:58 -0600

    innit

---
+++ Memorize/MemoryGame.swift
@@ -0,0 +1,20 @@
+//
+//  MemoryGame.swift
+//  Memorize
+//
+//  Created by His Dudeness on 6/30/21.
+//
+
+import Foundation
+
+struct MemoryGame<CardContent> {
+    var cards: Array<Card>
+    func choose(_ card: Card) {
+        
+    }
+    struct Card {
+        var isFaceUp: Bool
+        var isMatched: Bool
+        var content: CardContent
+    }
+}
