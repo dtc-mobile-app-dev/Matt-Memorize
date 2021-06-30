2c31e5ac0aba9e51477e1a7e50e4c3783fb7e8a5
hisDudeness95 <57687503+hisDudeness95@users.noreply.github.com>
2021-06-30 12:19:50 -0600

    innit

--- Memorize/EmojiMemoryGame.swift
+++ Memorize/EmojiMemoryGame.swift
@@ -7,8 +7,12 @@
 
 import SwiftUI
 
+func makeCardContent(index: Int) -> String {
+    return "🍆"
+}
+
 class EmojiMemoryGame {
-    private var model: MemoryGame<String> = MemoryGame<String>(numberOfPairsOfCards: 4)
+    private var model: MemoryGame<String> = MemoryGame<String>(numberOfPairsOfCards: 4) { index in "🍆"}
     
     var cards: Array<MemoryGame<String>.Card> {
         return model.cards
