77edbd94627a17566ede962eb9fefbee9716ce00
hisDudeness95 <57687503+hisDudeness95@users.noreply.github.com>
2021-07-01 09:53:40 -0600

    Update MemoryGame.swift

--- Memorize/MemoryGame.swift
+++ Memorize/MemoryGame.swift
@@ -10,7 +10,19 @@ import Foundation
 struct MemoryGame<CardContent> {
     private(set) var cards: Array<Card>
     func choose(_ card: Card) {
-        
+        let chosenIndex = index(of: card)
+        var chosenCard = cards[chosenIndex]
+        chosenCard.isFaceUp.toggle()
+        print("chosenCard = \(chosenCard)")
+    }
+
+    func index(of card: Card) -> Int {
+        for index in 0..<cards.count {
+            if cards[index].id == card.id {
+                return index
+            }
+        }
+         return 0
     }
     
     init(numberOfPairsOfCards: Int, createCardContent: (Int) -> CardContent) {
