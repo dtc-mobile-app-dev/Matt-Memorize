45e9e8c5f4ad2a945edb8bf87816723a23a35d4a
hisDudeness95 <57687503+hisDudeness95@users.noreply.github.com>
2021-07-26 11:46:02 -0600

    Update Themes.swift

--- Memorize/ViewModel/Themes.swift
+++ Memorize/ViewModel/Themes.swift
@@ -9,14 +9,15 @@ import SwiftUI
 
 struct Themes {
     
-    let themes: [Theme<String>]
-    
-    ["✈️", "🚀", "🚂", "🏎", "🛺", "🚜", "🛵", "🚟", "🚌", "🚕", "🚲", "🛴"], numberOfPairsOfCards: 10, themeName: "Vehicles", color: "red"
-    ["🍆", "🥦", "🧅", "🥔", "🌽", "🥕", "🫑", "🍅", "🧄", "🍠", "🥬", "🥒"], numberOfPairsOfCards: 14, themeName: "Veggies", color: "green"
-    ["⚽️", "🏈", "🎾", "⚾️", "🏐", "🏓", "🥍", "🥏", "🏀", "🏏", "🏒", "🤺"], numberOfPairsOfCards: 12, themeName: "Sports", color: "blue"
-    ["🥐", "🥯", "🍞", "🥖", "🥨", "🧇", "🥞", "🫓", "🥪", "🍕", "🍩", "🍪"], numberOfPairsOfCards: 12, themeName: "Delicious Bread", color: "yellow"
-    ["🎶", "🪘", "🪗", "🎷", "🎻", "🎺", "🎹", "🥁", "🎸", "🎤", "🎵", "🎼"], numberOfPairsOfCards: 12, themeName: "Music", color: "orange"
-    ["💽", "💾", "📞", "☎️", "📽", "📹", "📼", "📠", "📟", "🕹", "🖲", "💿"], numberOfPairsOfCards: 12, themeName: "Tech", color: "purple"
+    let themes: [Int : Theme<String>] =
+        [
+            0: Theme.init(content: ["✈️", "🚀", "🚂", "🏎", "🛺", "🚜", "🛵", "🚟", "🚌", "🚕", "🚲", "🛴"], color: "red", themeName: "Vehicles", numberOfPairsOfCards: 10),
+            1: Theme.init(content: ["🍆", "🥦", "🧅", "🥔", "🌽", "🥕", "🫑", "🍅", "🧄", "🍠", "🥬", "🥒"], color: "green", themeName: "veggies", numberOfPairsOfCards: 14),
+            2: Theme.init(content: ["⚽️", "🏈", "🎾", "⚾️", "🏐", "🏓", "🥍", "🥏", "🏀", "🏏", "🏒", "🤺"], color: "blue", themeName: "Sports", numberOfPairsOfCards: 12),
+            3: Theme.init(content: ["🥐", "🥯", "🍞", "🥖", "🥨", "🧇", "🥞", "🫓", "🥪", "🍕", "🍩", "🍪"], color: "yellow", themeName: "Delicious Bread", numberOfPairsOfCards: 12),
+            4: Theme.init(content: ["🎶", "🪘", "🪗", "🎷", "🎻", "🎺", "🎹", "🥁", "🎸", "🎤", "🎵", "🎼"], color: "orange", themeName: "Music", numberOfPairsOfCards: 12),
+            5: Theme.init(content: ["💽", "💾", "📞", "☎️", "📽", "📹", "📼", "📠", "📟", "🕹", "🖲", "💿"], color: "purple", themeName: "Tech", numberOfPairsOfCards: 12)
+        ]
 }
 
 //    if ContentCount <= numberOfPairsOfCards {
