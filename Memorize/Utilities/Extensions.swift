a9ac1d24205d8a187ecd282f296e2ceafe2bedef
hisDudeness95 <57687503+hisDudeness95@users.noreply.github.com>
2021-07-28 11:01:29 -0600

    innit

---
+++ Memorize/Utilities/Extensions.swift
@@ -0,0 +1,19 @@
+//
+//  Extensions.swift
+//  Memorize
+//
+//  Created by His Dudeness on 7/28/21.
+//
+
+import SwiftUI
+
+extension String {
+    func colorize() {
+        switch <#value#> {
+        case <#pattern#>:
+            <#code#>
+        default:
+            <#code#>
+        }
+    }
+}
