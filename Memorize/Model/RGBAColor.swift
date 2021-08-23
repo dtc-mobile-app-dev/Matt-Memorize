b8293ae91db9075de57d75df12621041632b3e48
hisDudeness95 <57687503+hisDudeness95@users.noreply.github.com>
2021-08-23 09:49:31 -0600

    Update RGBAColor.swift

--- Memorize/Model/RGBAColor.swift
+++ Memorize/Model/RGBAColor.swift
@@ -5,6 +5,30 @@
 //  Created by His Dudeness on 8/23/21.
 //
 
-import Foundation
+import SwiftUI
 
+struct RGBAColor: Codable, Equatable, Hashable {
+    let red: Double
+    let green: Double
+    let blue: Double
+    let alpha: Double
+}
 
+extension Color {
+    init(rgbaColor rgba: RGBAColor) {
+        self.init(.sRGB, red: rgba.red, green: rgba.green, blue: rgba.blue, opacity: rgba.alpha)
+    }
+}
+
+extension RGBAColor {
+    init(color: Color) {
+        var red: CGFloat = 0
+        var green: CGFloat = 0
+        var blue: CGFloat = 0
+        var alpha: CGFloat = 0
+        if let cgColor = color.cgColor {
+            UIColor(cgColor: cgColor).getRed(&red, green: &green, blue: &blue, alpha: &alpha)
+        }
+        self.init(red: Double(red), green: Double(green), blue: Double(blue), alpha: Double(alpha))
+    }
+}
