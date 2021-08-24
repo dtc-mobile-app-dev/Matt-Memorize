7b2f65480e6bfbe3140f35a84e5d057445879ee6
hisDudeness95 <57687503+hisDudeness95@users.noreply.github.com>
2021-08-24 10:10:45 -0600

    Update ThemeStore.swift

--- Memorize/ViewModel/ThemeStore.swift
+++ Memorize/ViewModel/ThemeStore.swift
@@ -10,7 +10,7 @@ import SwiftUI
 class ThemeStore: ObservableObject {
         let name: String
         
-    @Published var themed = [Themed]() {
+    @Published var theme = [Theme]() {
             didSet {
                 storeInUserDefaults()
             }
@@ -21,42 +21,42 @@ class ThemeStore: ObservableObject {
         }
         
         private func storeInUserDefaults() {
-            UserDefaults.standard.set(try? JSONEncoder().encode(themed), forKey: userDefaultsKey)
+            UserDefaults.standard.set(try? JSONEncoder().encode(theme), forKey: userDefaultsKey)
     //        UserDefaults.standard.set(palettes.map { [$0.name,$0.emojis,String($0.id)] }, forKey: userDefaultsKey )
         }
         
         private func restoreFromUserDefaults() {
             if let jsonData = UserDefaults.standard.data(forKey: userDefaultsKey),
-               let decodedThemes = try? JSONDecoder().decode(Array<Themed>.self, from: jsonData) {
-                themed = decodedThemes
+               let decodedThemes = try? JSONDecoder().decode(Array<Theme>.self, from: jsonData) {
+                theme = decodedThemes
             }
     }
     
     init(named name: String) {
         self.name = name
         restoreFromUserDefaults()
-        if themed.isEmpty {
-            insertThemed(named: <#T##String#>)
+        if theme.isEmpty {
+            insertTheme(named: <#T##String#>)
         }
     }
-    func themed(at index: Int) -> Themed {
-        let safeIndex = min(max(index, 0), themed.count - 1)
-        return themed[safeIndex]
+    func theme(at index: Int) -> Theme {
+        let safeIndex = min(max(index, 0), theme.count - 1)
+        return theme[safeIndex]
     }
     
     @discardableResult
     func removeTheme(at index: Int) -> Int {
-        if themed.count > 1, themed.indices.contains(index) {
-            themed.remove(at: index)
+        if theme.count > 1, theme.indices.contains(index) {
+            theme.remove(at: index)
         }
-        return index % themed.count
+        return index % theme.count
     }
     
-    func insertThemed(named name: String, emojis: String? = nil, at index: Int = 0) {
-        let unique = (themed.max(by: { $0.id < $1.id })?.id ?? 0) + 1
-        let themes = Themed(name: name, emojis: emojis ?? "", id: unique)
-        let safeIndex = min(max(index, 0), themed.count)
-        themed.insert(themes, at: safeIndex)
+    func insertTheme(named name: String, emojis: String? = nil, at index: Int = 0) {
+        let unique = (theme.max(by: { $0.id < $1.id })?.id ?? 0) + 1
+        let themes = Theme(name: name, emojis: emojis ?? "", id: unique)
+        let safeIndex = min(max(index, 0), theme.count)
+        theme.insert(themes, at: safeIndex)
     }
 
 }
