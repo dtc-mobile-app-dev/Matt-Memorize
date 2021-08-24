02d92f5c8f5bb687cd302a17cf35cefdfddaf2db
hisDudeness95 <57687503+hisDudeness95@users.noreply.github.com>
2021-08-24 10:38:05 -0600

    Update ThemeStore.swift

--- Memorize/ViewModel/ThemeStore.swift
+++ Memorize/ViewModel/ThemeStore.swift
@@ -7,10 +7,10 @@
 
 import SwiftUI
 
-class ThemeStore: ObservableObject {
+class ThemeStore<CardContent: Codable> : ObservableObject {
         let name: String
         
-    @Published var theme = [Theme<Any>]() {
+    @Published var themes = [Theme<CardContent>]() {
             didSet {
                 storeInUserDefaults()
             }
@@ -21,42 +21,42 @@ class ThemeStore: ObservableObject {
         }
         
         private func storeInUserDefaults() {
-            UserDefaults.standard.set(try? JSONEncoder().encode(theme), forKey: userDefaultsKey)
+            UserDefaults.standard.set(try? JSONEncoder().encode(themes), forKey: userDefaultsKey)
     //        UserDefaults.standard.set(palettes.map { [$0.name,$0.emojis,String($0.id)] }, forKey: userDefaultsKey )
         }
         
         private func restoreFromUserDefaults() {
             if let jsonData = UserDefaults.standard.data(forKey: userDefaultsKey),
-               let decodedThemes = try? JSONDecoder().decode(Array<Theme>.self, from: jsonData) {
-                theme = decodedThemes
+               let decodedThemes = try? JSONDecoder().decode(Array<Theme<CardContent>>.self, from: jsonData) {
+                themes = decodedThemes
             }
     }
     
     init(named name: String) {
         self.name = name
         restoreFromUserDefaults()
-        if theme.isEmpty {
+        if themes.isEmpty {
             
         }
     }
-    func theme(at index: Int) -> Theme<Any> {
-        let safeIndex = min(max(index, 0), theme.count - 1)
-        return theme[safeIndex]
+    func theme(at index: Int) -> Theme<CardContent> {
+        let safeIndex = min(max(index, 0), themes.count - 1)
+        return themes[safeIndex]
     }
     
     @discardableResult
     func removeTheme(at index: Int) -> Int {
-        if theme.count > 1, theme.indices.contains(index) {
-            theme.remove(at: index)
+        if themes.count > 1, themes.indices.contains(index) {
+            themes.remove(at: index)
         }
-        return index % theme.count
+        return index % themes.count
     }
     
     func insertTheme(named name: String, emojis: String? = nil, at index: Int = 0) {
-        let unique = (theme.max(by: { $0.id < $1.id })?.id ?? 0) + 1
-        let themes = Theme(name: name, emojis: emojis ?? "", id: unique)
-        let safeIndex = min(max(index, 0), theme.count)
-        theme.insert(themes, at: safeIndex)
+        let unique = (themes.max(by: { $0.id < $1.id })?.id ?? 0) + 1
+        let themes = Theme<CardContent>
+        let safeIndex = min(max(index, 0), themes.count)
+        themes.insert(themes, at: safeIndex)
     }
 
 }
