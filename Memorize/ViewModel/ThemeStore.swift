1b56b0bb52527132b8cffc74179ff31b829da7fc
hisDudeness95 <57687503+hisDudeness95@users.noreply.github.com>
2021-08-24 09:37:54 -0600

    Update ThemeStore.swift

--- Memorize/ViewModel/ThemeStore.swift
+++ Memorize/ViewModel/ThemeStore.swift
@@ -7,6 +7,49 @@
 
 import SwiftUI
 
-class ThemeStore: ObservableObject {
+struct Themed: Identifiable, Codable, Hashable {
+    var name: String
+    var emojis: String
+    var id: Int
     
+    fileprivate init(name: String, emojis: String, id: Int) {
+        self.name = name
+        self.emojis = emojis
+        self.id = id
+    }
+
+}
+
+class ThemeStore: ObservableObject {
+        let name: String
+        
+    @Published var themes = [Themed] {
+            didSet {
+                storeInUserDefaults()
+            }
+        }
+        
+        private var userDefaultsKey: String {
+            "ThemeStore:" + name
+        }
+        
+        private func storeInUserDefaults() {
+            UserDefaults.standard.set(try? JSONEncoder().encode(themes), forKey: userDefaultsKey)
+    //        UserDefaults.standard.set(palettes.map { [$0.name,$0.emojis,String($0.id)] }, forKey: userDefaultsKey )
+        }
+        
+        private func restoreFromUserDefaults() {
+            if let jsonData = UserDefaults.standard.data(forKey: userDefaultsKey),
+               let decodedThemes = try? JSONDecoder().decode(Array<Theme>.self, from: jsonData) {
+                themes = decodedThemes
+            }
+    }
+    
+    init(named name: String) {
+        self.name = name
+        restoreFromUserDefaults()
+        if themes.isEmpty {
+            
+        }
+    }
 }
