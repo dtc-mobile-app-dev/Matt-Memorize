8c45936ef8be715735e7440b22a6e8f8408ac6bc
hisDudeness95 <57687503+hisDudeness95@users.noreply.github.com>
2021-08-24 12:05:53 -0600

    innit

--- Memorize/View/ThemeChooser.swift
+++ Memorize/View/ThemeChooser.swift
@@ -2,11 +2,19 @@
 //  ThemeChooser.swift
 //  Memorize
 //
-//  Created by His Dudeness on 8/19/21.
+//  Created by His Dudeness on 8/24/21.
 //
 
 import SwiftUI
 
-class ThemeChooser: ObservableObject {
-    
+struct ThemeChooser: View {
+    var body: some View {
+        List()
+    }
+}
+
+struct ThemeChooser_Previews: PreviewProvider {
+    static var previews: some View {
+        ThemeChooser()
+    }
 }
--- Memorize/ViewModel/Themes.swift
+++ Memorize/ViewModel/Themes.swift
@@ -10,13 +10,13 @@ import SwiftUI
 struct Themes {
     
     static let themes: [Int : Theme<String>] =
-        [
-            0: Theme.init(content: ["✈️", "🚀", "🚂", "🏎", "🛺", "🚜", "🛵", "🚟", "🚌", "🚕", "🚲", "🛴"], color: "red", themeName: "Vehicles", numberOfPairsOfCards: 10),
-            1: Theme.init(content: ["🍆", "🥦", "🧅", "🥔", "🌽", "🥕", "🫑", "🍅", "🧄", "🍠", "🥬", "🥒"], color: "green", themeName: "veggies", numberOfPairsOfCards: 14),
-            2: Theme.init(content: ["⚽️", "🏈", "🎾", "⚾️", "🏐", "🏓", "🥍", "🥏", "🏀", "🏏", "🏒", "🤺"], color: "blue", themeName: "Sports", numberOfPairsOfCards: 12),
-            3: Theme.init(content: ["🥐", "🥯", "🍞", "🥖", "🥨", "🧇", "🥞", "🫓", "🥪", "🍕", "🍩", "🍪"], color: "yellow", themeName: "Delicious Bread", numberOfPairsOfCards: 12),
-            4: Theme.init(content: ["🎶", "🪘", "🪗", "🎷", "🎻", "🎺", "🎹", "🥁", "🎸", "🎤", "🎵", "🎼"], color: "orange", themeName: "Music", numberOfPairsOfCards: 12),
-            5: Theme.init(content: ["💽", "💾", "📞", "☎️", "📽", "📹", "📼", "📠", "📟", "🕹", "🖲", "💿"], color: "purple", themeName: "Tech", numberOfPairsOfCards: 12)
+        [:
+//            0: Theme.init(content: ["✈️", "🚀", "🚂", "🏎", "🛺", "🚜", "🛵", "🚟", "🚌", "🚕", "🚲", "🛴"], color: "red", themeName: "Vehicles", numberOfPairsOfCards: 10),
+//            1: Theme.init(content: ["🍆", "🥦", "🧅", "🥔", "🌽", "🥕", "🫑", "🍅", "🧄", "🍠", "🥬", "🥒"], color: "green", themeName: "veggies", numberOfPairsOfCards: 14),
+//            2: Theme.init(content: ["⚽️", "🏈", "🎾", "⚾️", "🏐", "🏓", "🥍", "🥏", "🏀", "🏏", "🏒", "🤺"], color: "blue", themeName: "Sports", numberOfPairsOfCards: 12),
+//            3: Theme.init(content: ["🥐", "🥯", "🍞", "🥖", "🥨", "🧇", "🥞", "🫓", "🥪", "🍕", "🍩", "🍪"], color: "yellow", themeName: "Delicious Bread", numberOfPairsOfCards: 12),
+//            4: Theme.init(content: ["🎶", "🪘", "🪗", "🎷", "🎻", "🎺", "🎹", "🥁", "🎸", "🎤", "🎵", "🎼"], color: "orange", themeName: "Music", numberOfPairsOfCards: 12),
+//            5: Theme.init(content: ["💽", "💾", "📞", "☎️", "📽", "📹", "📼", "📠", "📟", "🕹", "🖲", "💿"], color: "purple", themeName: "Tech", numberOfPairsOfCards: 12)
         ]
 }
 
