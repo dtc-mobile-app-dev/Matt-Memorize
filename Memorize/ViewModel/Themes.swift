40a60852f73db5c7979dfd4da9f34cdb34e0745b
hisDudeness95 <57687503+hisDudeness95@users.noreply.github.com>
2021-08-30 09:03:46 -0600

    Update Themes.swift

--- Memorize/ViewModel/Themes.swift
+++ Memorize/ViewModel/Themes.swift
@@ -13,10 +13,10 @@ struct Themes {
         [
             Theme.init(content: ["✈️", "🚀", "🚂", "🏎", "🛺", "🚜", "🛵", "🚟", "🚌", "🚕", "🚲", "🛴"], color: RGBAColor(red: 1.0, green: 0.0, blue: 0.0,alpha: 1.0), themeName: "Vehicles", pairsOfCards: 10, id: 0),
             Theme.init(content: ["🍆", "🥦", "🧅", "🥔", "🌽", "🥕", "🫑", "🍅", "🧄", "🍠", "🥬", "🥒"], color: RGBAColor(red: 0.0, green: 1.0, blue: 0.0,alpha: 1.0), themeName: "Veggies", pairsOfCards: 14, id: 1),
-            Theme.init(content: ["⚽️", "🏈", "🎾", "⚾️", "🏐", "🏓", "🥍", "🥏", "🏀", "🏏", "🏒", "🤺"], color: RGBAColor(red: 0.0, green: 0.0, blue: 1.0,alpha: 1.0), themeName: "Sports", pairsOfCards: 12, id: 2),
+            Theme.init(content: ["⚽️", "🏈", "🎾", "⚾️", "🏐", "🏓", "🥍", "🥏", "🏀", "🏏", "🏒", "🤺"], color: RGBAColor(red: 0.25, green: 0.25, blue: 0.95,alpha: 1.0), themeName: "Sports", pairsOfCards: 12, id: 2),
             Theme.init(content: ["🥐", "🥯", "🍞", "🥖", "🥨", "🧇", "🥞", "🫓", "🥪", "🍕", "🍩", "🍪"], color: RGBAColor(red: 0.25, green: 0.5, blue: 0.25,alpha: 1.0), themeName: "Delicious Bread", pairsOfCards: 12, id: 3),
             Theme.init(content: ["🎶", "🪘", "🪗", "🎷", "🎻", "🎺", "🎹", "🥁", "🎸", "🎤", "🎵", "🎼"], color: RGBAColor(red: 0.5, green: 0.0, blue: 0.5,alpha: 1.0), themeName: "Music", pairsOfCards: 12, id: 4),
-            Theme.init(content: ["💽", "💾", "📞", "☎️", "📽", "📹", "📼", "📠", "📟", "🕹", "🖲", "💿"], color: RGBAColor(red: 1.0, green: 0.0, blue: 1.0,alpha: 1.0), themeName: "Tech", pairsOfCards: 12, id: 5)
+            Theme.init(content: ["💽", "💾", "📞", "☎️", "📽", "📹", "📼", "📠", "📟", "🕹", "🖲", "💿"], color: RGBAColor(red: 1.0, green: 0.5, blue: 1.0,alpha: 1.0), themeName: "Tech", pairsOfCards: 12, id: 5)
         ]
 }
 
