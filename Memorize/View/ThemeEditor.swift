c2583b9ad9846f5c11f05760ebc018189024d2ea
hisDudeness95 <57687503+hisDudeness95@users.noreply.github.com>
2021-08-30 12:47:25 -0600

    Update ThemeEditor.swift

--- Memorize/View/ThemeEditor.swift
+++ Memorize/View/ThemeEditor.swift
@@ -10,12 +10,16 @@ import SwiftUI
 struct ThemeEditor: View {
     @Binding var theme: Theme<String>
     
-    
     var body: some View {
         Form {
             
         }
+        .navigationTitle("Edit \(theme.themeName)")
+        .frame(minWidth: 300, minHeight: 350)
     }
+    var nameSection: some View {
+        Section(header: Text("Name")) {
+            TextField("Name", text: $theme.themeName)
 }
 
 //struct ThemeEditor_Previews: PreviewProvider {
